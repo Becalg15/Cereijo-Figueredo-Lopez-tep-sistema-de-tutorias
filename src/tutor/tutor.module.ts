fcf782a0e1ad7eb7f97e5303db32d1872cf128fa
Beca <mrlopez.21@est.ucab.edu.ve>
2025-07-09 13:43:52 -0400

    chore: Registro de tutor, obtención de perfil de tutor, actualización de pergil de tutor

--- src/tutor/tutor.module.ts
+++ src/tutor/tutor.module.ts
@@ -4,11 +4,16 @@ import { TutorController } from './tutor.controller';
 import { TutorService } from './tutor.service';
 import { Tutor } from './entities/tutor.entity';
 import { Usuario } from '../users/entities/usuario.entity';
+import { Materia } from '../materia/entities/materia.entity'; 
+import { MateriaModule } from '../materia/materia.module'; 
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Tutor, Usuario])],
+  imports: [
+    TypeOrmModule.forFeature([Tutor, Usuario, Materia]), 
+    MateriaModule, 
+  ],
   controllers: [TutorController],
   providers: [TutorService],
-  exports:[TutorService],
+  exports: [TutorService],
 })
 export class TutorModule {}
