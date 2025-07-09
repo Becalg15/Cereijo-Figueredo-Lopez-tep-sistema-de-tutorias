6077b4c02f7f710363a6bb44b19b3be6832ebb86
Beca <mrlopez.21@est.ucab.edu.ve>
2025-07-08 23:08:13 -0400

    chore:creación de estudiante y obtención del perfil del estudiante

--- src/estudiante/estudiante.controller.ts
+++ src/estudiante/estudiante.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  UsePipes,
+  ValidationPipe,
+  HttpCode,
+  HttpStatus,
+  Get,
+  UseGuards,
+} from '@nestjs/common';
+import { EstudianteService } from './estudiante.service';
+import { CreateEstudianteDto } from './dto/create-estudiante.dto';
+import { Estudiante } from './entities/estudiante.entity';
+import { AuthGuard } from '@nestjs/passport';
+import { UsuarioActual } from '../auth/decorators/user.decorator';
 
-@Controller('estudiante')
-export class EstudianteController {}
+@Controller('estudiantes')
+export class EstudianteController {
+  constructor(private readonly estudianteService: EstudianteService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  @UsePipes(new ValidationPipe({ whitelist: true }))
+  async create(@Body() dto: CreateEstudianteDto): Promise<Estudiante> {
+    return this.estudianteService.create(dto);
+  }
+
+  @Get('perfil')
+  @UseGuards(AuthGuard('jwt'))
+  async obtenerPerfil(@UsuarioActual() usuario: any) {
+    return this.estudianteService.obtenerPerfil(usuario.id);
+  }
+}
